04f01cfebafe10f25767c58b79e3f5590d61905b
7005215-wmci <7005215@spsd.sk.ca>
2025-09-29 09:13:10 -0600

    Erasure

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -4,24 +4,13 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let switches = 1;
-let color = "black"
 
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(color)
-  if (millis() > 2000 * switches + 28) {
-    if (color === "yellow") {
-      color = "black";
-    }
-    
-    else {
-      color = "yellow";
-    }
-    switches = switches + 1;
-  }
-  
+  background(220);
+  fill("white");
+  rect(x, y, w, h)
 }
