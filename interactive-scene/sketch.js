eb997f3905804771879ec28506348e0eb758ab40
7005215-wmci <7005215@spsd.sk.ca>
2025-09-29 09:53:59 -0600

    Drawing System Works!

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,50 +1,20 @@
-// CS30 Interactive Scene
+// Basic Drawing Program
 // Kalim Pugh
 // 9/29/2025
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let cellSize;
 
 function setup() {
-  if (windowWidth > windowHeight) {
-    createCanvas(windowHeight, windowHeight);
-  }
-  else {
-    createCanvas(windowWidth, windowWidth)
-  }
-  cellSize = width/8
+  createCanvas(windowWidth,windowHeight);
+  background("white");
 }
 
 function draw() {
-  background(220);
-  chessboard();
+  fill("black");
 }
 
-function windowResized() {
-  if (windowWidth > windowHeight) {
-    createCanvas(windowHeight, windowHeight);
-  }
-  else {
-    createCanvas(windowWidth, windowWidth)
-  }
-  cellSize = width/8
-}
-
-function chessboard() {
-  let isWhite = true;
-  for (let x = 0; x < 8; x++) {
-    for (let y = 0; y<8; y++){
-      if (isWhite) {
-        fill("white")
-      }
-      else{
-        fill("black")
-      }
-      square(x*cellSize, y*cellSize, cellSize);
-      isWhite = !isWhite;
-    }
-    isWhite = !isWhite
-  }
+function mouseClicked() {
+  circle(mouseX, mouseY, 15);
 }
