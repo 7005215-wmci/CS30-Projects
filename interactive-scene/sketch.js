68f30c1c8d118b67f558eccb10bb4c74702bd0b5
7005215-wmci <7005215@spsd.sk.ca>
2025-10-01 09:33:17 -0600

    Update sketch.js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,15 +6,67 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let shape = true;
+let size = 10;
+let colors = "black";
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background("white");
 }
 
 function draw() {
-  fill("black");
+  fill(colors);
+  noStroke();
 }
 
-function mouseClicked() {
-  circle(mouseX, mouseY, 15);
+function mouseDragged() {
+  if (shape === true){
+    square(mouseX, mouseY, size);
+  }
+
+  else {
+    circle(mouseX, mouseY, size);
+  }
+  
+}
+
+function keyPressed(){
+  if (key === "r"){
+    background("white");
+  }
+  
+  else if (key === "q"){
+    shape = !shape;
+  }
+  
+  else if (key === "e") {
+    size = size + 10;
+  }
+  
+  else if (key === "w") {
+    size = size - 10;
+  }
+  
+  else if  (key === "t") {
+    if (colors === "black"){
+      colors = "blue";
+    }
+    
+    else if (colors === "blue") {
+      colors = "red";
+    }
+    
+    else if (colors === "red"){
+      colors = "green";
+    }
+    
+    else if (colors === "green") {
+      colors = "yellow";
+    }
+    
+    else {
+      colors = "black";
+    }
+  }
 }
