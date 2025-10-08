d5bc9fb50d78944ec451b263a86703ae02335e3f
7005215-wmci <7005215@spsd.sk.ca>
2025-10-08 09:31:39 -0600

    Update sketch.js

--- bouncing-ball/sketch.js
+++ bouncing-ball/sketch.js
@@ -5,47 +5,60 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x;
-let y;
-let dx; //or use xSpeed
-let dy;
-let r;
-let g;
-let b;
-let radius = 75;
+
+let theBallArray = [];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   noStroke();
-  x = width/2;
-  y = height/2;
-  dx = random(-3, 3);
-  dy = random(-3, 3);
-  randomizeColor();
+  // randomizeColor();
+  spawnBall();
 }
 
 function draw() {
-  background("red");
+  background("black");
   moveCircle();
   bounceIfNeeded();
   showCircle();
   // console.log(y);
 }
 
+function mousePressed() {
+  spawnBall();
+}
+
+function spawnBall() {
+  let newBall = {
+    x: width/2,
+    y: height/2,
+    dx: random(-3, 3),
+    dy: random(-3, 3),
+    radius: random(25, 75),
+    r: random(255),
+    g: random(255),
+    b: random(255),
+  };
+  theBallArray.push(newBall);
+}
+
 function moveCircle() {
-  x = x + dx;
-  y += dy;
+  for (let ball of theBallArray) {
+    ball.x = ball.x + ball.dx;
+    ball.y += ball.dy;
+  }
 }
 
 function bounceIfNeeded() {
-  //should i bounce?
-  if (x < 0 + radius || x > width - radius) {
-    dx = dx * -1;
-    randomizeColor();
-  }
-  if (y < 0 + radius || y > height - radius) {
-    dy = dy * -1;
-    randomizeColor();
+  for (let ball of theBallArray) {
+    //should i bounce?
+    if (ball.x < 0 + ball.radius || ball.x > width - ball.radius) {
+      ball.dx = ball.dx * -1;
+      // randomizeColor();
+    }
+    if (ball.y < 0 + ball.radius || ball.y > height - ball.radius) {
+      ball.dy = ball.dy * -1;
+      // randomizeColor();
+    }
   }
 }
 
@@ -56,6 +69,8 @@ function randomizeColor() {
 }
 
 function showCircle() {
-  fill(r, g, b);
-  circle(x, y, radius*2);
+  for (let ball of theBallArray) {
+    fill(ball.r, ball.g, ball.b);
+    circle(ball.x, ball.y, ball.radius*2);
+  }
 }
