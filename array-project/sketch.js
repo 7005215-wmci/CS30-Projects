5ccdc77e822169264f3454fb64cee12746b007ac
7005215-wmci <7005215@spsd.sk.ca>
2025-10-22 09:53:12 -0600

    Centering + Terrain
    Added a Terrarain Variable to change speed if it was standing on the mountain or grass. Centered the character and reworked code regarding that to make movement more smooth.

--- array-project/sketch.js
+++ array-project/sketch.js
@@ -5,15 +5,17 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x = 0;
-let y = 0;
+let x = 100;
+let y = 100;
 let oldY;
 let oldX;
 let pStats = {
   speed: 5,
   color: "black",
   direction: "north",
-
+  terrarain: "flat",
+  tSpeed: 1,
+  energy: 100,
 };
 
 function setup() {
@@ -22,36 +24,47 @@ function setup() {
 }
 
 function draw() {
+  if (x > 275 && y > 275){
+    pStats.tSpeed = 0.4;
+  }
+  else {
+    pStats.tSpeed = 1;
+  }
   background(34, 139, 34);
   fill( '#857F7B' );
   square(300, 300, 200);
   if(keyIsDown(87) === true){
     oldY= y;
-    y -= pStats.speed;
+    y -= pStats.speed * pStats.tSpeed;
     pStats.direction = "north";
   }
   
   if(keyIsDown(83) === true){
     oldY= y;
-    y += pStats.speed;
+    y += pStats.speed * pStats.tSpeed;
     pStats.direction = "south";
   }
 
   if(keyIsDown(65) === true){
     oldX = x;
-    x -= pStats.speed;
+    x -= pStats.speed * pStats.tSpeed;
     pStats.direction = "west";
   }
 
   if(keyIsDown(68) === true){
     oldX =x;
-    x += pStats.speed;
+    x += pStats.speed * pStats.tSpeed;
     pStats.direction = "east";
   }
+  movement();
+}
+
+function movement() {
   fill("black");
-  if(x < 0 || y < 0 || x > 450|| y > 450) {
+  if(x < 22 || y < 25 || x > 475|| y > 475) {
     x = oldX;
     y = oldY;
   }
-  square(x ,y , 50);
+  square(x - 25 ,y - 25 , 50);
+  console.log(pStats.direction);
 }
